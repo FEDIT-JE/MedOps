1a0a860f4e00acbb95851c7ed84d6b6303f6d74b
Jan Eggenberger <j.eggenberger@federerag.ch>
2025-06-02 12:33:41 +0200

    login test

---
+++ MedOps/MedOps/MedOps/Pages/Login.razor.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+namespace MedOps.Pages
+{
+    public partial class Login
+    {
+
+        private bool success;
+        private string[] errors = new string[0];
+        private bool isBusy = false;
+
+        private string email = string.Empty;
+        private string password = string.Empty;
+
+        private bool isLoggedIn = false;
+        private string message = string.Empty;
+
+        private async Task LoginAsync()
+        {
+            isBusy = true;
+            await Task.Delay(2500);
+            if (email.Equals("test@test.com") && password == "Test.123")
+            {
+                isLoggedIn = true;
+                message = "Welcome to MudBlazor Sample from AK, I hope you liked it";
+            }
+            else
+            {
+                isLoggedIn = false;
+                message = "Invalid email or password";
+            }
+            isBusy = false;
+        }
+    }
+}
